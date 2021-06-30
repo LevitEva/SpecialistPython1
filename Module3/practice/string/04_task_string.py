933414572c7930b5d6362fc92e26d6c488c99784
LevitEva <86655315+LevitEva@users.noreply.github.com>
2021-06-30 12:22:24 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,13 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+t=0
+word_len=0
+while t < len(text):
+    if text[t] ==" ":
+        print(word_len)
+        word_len = 0
+    else:
+        word_len += 1
+    t += 1
+print(num_long_word)

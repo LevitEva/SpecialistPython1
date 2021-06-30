e258356a48928e782cf980c75c1b2fb31f8920cd
LevitEva <86655315+LevitEva@users.noreply.github.com>
2021-06-30 15:13:25 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+my_list=[1,7,6,5,-4,-3]
+summa=0
+for el in my_list:
+    summa = summa + el
+    print(summa)
+    print(sum(my_list))

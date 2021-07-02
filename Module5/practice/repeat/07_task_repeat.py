cc0fd9720a671dc5f78f4c982180d587a0fb2479
LevitEva <86655315+LevitEva@users.noreply.github.com>
2021-07-02 12:23:22 +0300

    Update 07_task_repeat.py

--- Module5/practice/repeat/07_task_repeat.py
+++ Module5/practice/repeat/07_task_repeat.py
@@ -5,3 +5,12 @@ def deposit(x, a, n):
     pass
 # функция принимает три числа и возвращает одно - итоговая сумма на счету Ивана.
 # Проценты на вклад начисляются один раз в год.
+def last_page(num_items, items_on_page):
+    last_items = num_items % items_on_page
+    if last_items == 0:
+        last_items = items_on_page
+    return last_items
+
+
+print(last_page(123, 10))
+print(last_page(120, 10))
